fed8f0a97c62e6677ebba58cdd0c1c775e9eaf0c
nkpro2000 <45198561+nkpro2000@users.noreply.github.com>
2020-04-23 00:03:35 +0530

    Create updateREADME.py

---
+++ .github/workflows/updateREADME.py
@@ -0,0 +1,10 @@
+import re
+
+with open('content','r') as content:
+  contents = content.read().splitlines()
+
+with open('README.md','r') as readme:
+  pre = readme.read()
+
+with open('README.md','w') as readme:
+  readme.write(readme.write(re.sub('```content[\s\S]*```','```content\n'+'\n'.join(contents)+'\n```',pre))
